01c3e0f943b6ccda2b8be70ed7575383218dd248
Robbie <regbelen@addu.edu.ph>
2025-11-12 23:22:07 +0800

    WORKING SKIP

--- src/main/java/com/mycompany/vibra/Content/Main_Page/Main_Contents/Music_Player/MainLibraryPanel.java
+++ src/main/java/com/mycompany/vibra/Content/Main_Page/Main_Contents/Music_Player/MainLibraryPanel.java
@@ -71,8 +71,10 @@ public class MainLibraryPanel extends JPanel implements ThemeManager.ThemeChange
 
         ThemeManager.getInstance().addThemeChangerListener(this);
         applyTheme();
+        // ‼️ CHANGE THIS PATH
+        String hardcodedScanPath ="C:/Users/robbi/Music/vibramusic-20251112T143148Z-1-001/vibramusic";
 
-        String hardcodedScanPath = "/Users/eeeuweee/Music/vibramusic"; // ‼️ CHANGE THIS PATH
+
         new ScanWorker(hardcodedScanPath).execute();
     }
 
